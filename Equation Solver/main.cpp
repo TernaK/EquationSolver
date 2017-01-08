7c0b695e07384ad75cc8f24215bb5560d6c61612
Terna Kpamber <tkpamber@yahoo.com>
2017-01-07 23:45:15 -0800

    Initial Commit

---
+++ Equation Solver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Equation Solver
+//
+//  Created by Terna Kpamber on 1/7/17.
+//  Copyright © 2017 Terna Kpamber. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
