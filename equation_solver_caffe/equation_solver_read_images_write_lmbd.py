1ad83cb5bf7efbb6e0b84ea07c246e15ade1527a
Terna <tkpamber@yahoo.com>
2017-01-28 13:17:38 -0800

    Remove models, new data script

---
+++ equation_solver_caffe/equation_solver_read_images_write_lmbd.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+import lmdb
+import caffe
+# Let's pretend this is interesting data
+#X = np.zeros((N, 1, 28, 28), dtype=np.uint8)
+#y = np.zeros(N, dtype=np.int64)
+
+# We need to prepare the database for the size. We'll set it 10 times
+# greater than what we theoretically need. There is little drawback to
+# setting this too big. If you still run into problem after raising
+# this, you might want to try saving fewer entries in a single
+# transaction.
+def readImageWriteLmdb(readPath, index):
+    with env.begin(write=True) as txn:
+        #print(readPath)
+        X = cv2.imread(readPath, cv2.IMREAD_GRAYSCALE)
+        datum = caffe.proto.caffe_pb2.Datum()
+        datum.channels = 1
+        datum.height = 28
+        datum.width = 28
+        datum.data = X.tostring()  # or .tostring() if numpy < 1.9
+        datum.label = labels[p]
+        str_id = '{:08}'.format(index)
+        index = index + 1
+        txn.put(str_id.encode('ascii'), datum.SerializeToString())
+        return
+
+N = 13*13*14
+
+map_size = N*10 
+
+env = lmdb.open('equation_solver_lmdb', map_size=map_size)
+
+index = 0
+
+#get operators
+path1 = 'operators/'
+path2 = ['plus/', 'minus/', 'divide/', 'multiply/']
+labels = [10,11,12,13]
+
+for p in range(4):
+    for i in range(N/14):
+        readImageWriteLmdb(path1+path2[p]+str(i)+'.png', index)
+        index += 1
+        
+#get operands
+path1 = 'operands/'
+for p in range(10):
+    for i in range(N/14):
+        readImageWriteLmdb(path1+str(p)+'/'+str(i)+'.png', index)
+        index += 1
